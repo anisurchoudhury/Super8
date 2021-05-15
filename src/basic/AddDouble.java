6cbd961f3fce012fe6f2adcbb4d4648c0513f10f
anischoudhury <anischoudhury@10.67.105.193>
2021-05-15 16:38:33 -0400

    first commit

---
+++ src/basic/AddDouble.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class AddDouble {
+
+	public static void main(String[] args) {
+String name;
+name="Anis";
+System.out.print("Hello, My name is "+name);
+
+	}//end of main
+
+}//and of AddDouble
---
+++ src/basic/HelloWorld.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+System.out.println("Hello World");
+System.out.println("My Name is Anis");
+System.out.println("My Address is");
+System.out.println("5037 43rd St");
+System.out.println("Woodside, NY 11377");
+	 
+	
+	}//end of main
+
+}//end of HelloWorld
